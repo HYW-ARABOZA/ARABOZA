e6c613cdcc8ba106996f5d974af4dc0419850188
TjDbsws <132185400+TjDbsws@users.noreply.github.com>
2023-07-22 10:47:39 +0900

    Revert "0722_홍서윤 소셜 로그인 구현 완료"

--- accounts/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
-from django.contrib.auth import get_user_model
-
-class UserForm(UserCreationForm):
-    class Meta:
-        model = get_user_model()
-        fields = ['nickname', 'username', 'email']
