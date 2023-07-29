b06f761479e6bcae206745695439ffeee8bb1fd9
hootzz <wjqwjql3@gmail.com>
2023-07-30 01:59:49 +0900

    [0729] 작성 기능 수정, 로긍니 수정 홍윤기

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from django.contrib.auth.hashers import make_password, check_password
 from rest_framework.response import Response
@@ -30,6 +31,8 @@ class Join(APIView):
         return Response(status=200, data=dict(message='회원가입 성공!'))
 
 # 로그인
+
+@login_required
 class Login(APIView):
     def get(self, request):
         return render(request, 'accounts/login.html')
@@ -55,9 +58,9 @@ class Login(APIView):
         request.session['email'] = user.email
 
         return Response(status=200, data=dict(message='로그인 성공!'))
-        return HttpResponseRedirect(reverse('diary:mainIndex'))
 
 
+@login_required
 def login(request):
     if request.method == "GET":
         return render(request, 'accounts/login.html')
@@ -75,16 +78,21 @@ def login(request):
         user = authenticate(request, email=email, password=password)
 
         if user is not None:
+            request.session['loginCheck'] = True
+            request.session['email'] = user.email
             login(request, user)
-            return Response(status=200, data=dict(message='로그인 성공!'))
-            return HttpResponseRedirect(reverse('diary:mainIndex.html'))
+            return render(request, 'diary/mainIndex.html', context='로그인 성공!')
 
         elif user is None:
-            return Response(status=500, data=dict(messag='입력정보가 올바르지않습니다.'))
+            return Response(status=500, data=dict(messag='입력정보가 올바르지 않습니다.'))
 
         elif check_password(password, user.password) is False:
             return Response(status=500, data=dict(message='비밀번호가 올바르지 않습니다.'))
 
+def logout(request):
+    if request.session.get('user'):
+        del(request.session['user'])
+    return redirect('/')
 
 
 
